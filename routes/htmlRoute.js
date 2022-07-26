909ce4ca88881f72d95cbad5f8578617d9fefbc0
dubsumm <w.b.summerlin@gmail.com>
2022-07-25 21:34:13 -0400

    functional note reading and writing added

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -2,15 +2,18 @@ const path = require('path');
 const router  = require('express').Router();
 const fs = require('fs');
 
+router.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+);
 
 //handles the notes.html
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname,'../public/notes.html'))
-})
+});
 //handles all others
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+});
 
 
 module.exports = router
--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,43 +1,41 @@
-const router = require('express').Router();
-const fs = require('fs');
+const router = require("express").Router();
+const fs = require("fs");
+const uuid = require("../helpers/uuid");
 
 //GET request
-router.get('/notes', (req, res) => {
-    
-        let response = fs.readFileSync('./db/db.json');
-        response = JSON.parse(response);
-        res.json(response);
-        console.log(response);
-        
-})
+router.get("/notes", (req, res) => {
+  let response = fs.readFileSync("./db/db.json");
+  response = JSON.parse(response);
+  res.json(response);
+  console.log(response);
+});
 
 // POST request
-router.post('/notes', (req, res) => {
+router.post("/notes", (req, res) => {
+  const { title, text } = req.body;
 
-    // this stuff is from activity 15 from express
-//     console.info(`${req.method} request received to add a review`);
-
-//   // Prepare a response object to send back to the client
-//   let response;
-
-//   // Check if there is anything in the response body
-//   if (req.body && req.body.product) {
-//     response = {
-//       status: 'success',
-//       data: req.body,
-//     };
-//     res.json(`Review for ${response.data.product} has been added!`);
-//   } else {
-//     res.json('Request body must at least contain a product name');
-//   }
-
-
-    console.log(req.body);
-
-
-
-})
+  if (title && text) {
+    const newNote = { title, text, id: uuid() };
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        let parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(parsedNotes, null, 2), (throwError) =>
+        throwError ? console.error(throwError) : console.info('Posted Note!'));
+      }
+    });
+    const response = {
+      status: "VUNDERBAR",
+      body: newNote,
+    };
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Make sure to add a title or text');
+  };
+});
 
 // DELETE request
 
-module.exports = router
+module.exports = router;
